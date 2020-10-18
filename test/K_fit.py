0c0bb2a7a9ad301aca82f830deaee7060677cd84
mysarapa <66000392+mysarapa@users.noreply.github.com>
2020-10-18 09:33:06 +0200

    Update K_fit.py

--- test/K_fit.py
+++ test/K_fit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 # Created on Tue Mar  3 14:57:41 2020
@@ -6,47 +7,48 @@
 
 # This software is released under the GNU General Public License
 # Please cite:
-# Kania A., Wójcik-Augustyn A., Sarapata K., Gucwa M., Murzyn K.,
+# Kania A., Wójcik-Augustyn A., Sarapata K., Gucwa M., 
 # An algebraic approach to determine the torsional coefficients - refinement of OPLS-AA force field for dimethyl phosphate molecule,
-# Acta Biochimica Polonica 2020
+# 2020
 
 Analytical solution of dihedral fitting
 
-The script ‘analytical_fit_dihedral.py’ calculates (fits) Fourier parameters for dihedral energy part (in force field equation).
+The script ‘K_fit.py’ calculates (fits) Fourier parameters for dihedral energy part (in force field equation).
 As a input it takes the following data files. First, the energy of a molecule for different conformations is demanded.
 It can be obtained using quantum chemistry methods. The second file should include energies for the same states 
 but without dihedral part (parameters are set to zero). The last input files consist of dihedral angles of a molecule
 which were considered (one dih file for every dihedral angle).
 
-According to the formula (link), the script calculates the estimated value of torsion part based on the given energy values.
+According to the formula in cited paper, the script calculates the estimated value of torsion part based on the given energy values.
 The calculated parameters are selected in that way to minimize the square of the difference between corresponding energies
 from the first and second file.
 All torsion angles can be grouped according to their types.
 For instance the torsion angle formed from OH.8-P.6-OS.10-CT atoms belongs to the same group as this one: CT.1-OS.5-P.6-OH.8.
 
 The same set of four parameters K1, K2, K3, K4 applies to every dihedral of a given type.
-The ‘analytical_fit_dihedral.py’ script optionally expects two values ‘from’ and ‘to’ 
+The ‘K_fit.py’ script optionally expects two values ‘from’ and ‘to’ 
 which set the range of the resulting parameters K. These optionally values 
 and mandatory paths to data files the script gets from the standard input or text file with paths to QM energy,
 MM energy and all dihedral angles files.
-In the result, the script returns the set of four optimal Fourier parameters for each dihedral types 
-and optionally set of four Fourier parameters from declared range for each dihedral types.
+In the result, the script returns the set of four optimal Fourier or Ryckaert-Belleman parameters for each dihedral types 
+and optionally set of four Fourier parameters from declared range for each dihedral types. 
+The cited work includes the method of obtaining Ryckaert-Belleman parameters based on Fourier ones.
 
 
 usage:
 
-./K_fit.py from to < paths2files.txt  > cofficients.out
+./K_fit.py from to < paths2files.txt  > coefficients.out
 
 option:
 
 from, to	    optionally two values ‘from’ and ‘to’ which set the range of the resulting parameters K.
-				The calculated K parameters should be included inside this range.
+				The calculated K parameters should be included inside this range. This option is only for Fourier parameters.
 
 path2files.txt  includes paths to data files: 
                      path to QM energy file,
                      MM energy file,
                      *.dih files
-                     and the amount of cofficients to calculate (3 or 4 value is allowed).
+                     and the amount of coefficients to calculate (3 or 4 value is allowed).
 
 data files:
 
@@ -56,12 +58,13 @@ MM energy file	the energy of a molecule for different conformations calculated w
 				Values should have column layout.
 
 
-
 *.dih files 	with values of all dihedrals in molecule for different conformations. Values should have column layout. 
 				The first line in each files containing the type of dihedral: order of the atoms forming that angle. 
 				e.g. 'P.6-OS.10-CT.11-HC.14.dih' file in first line should have: 'P OS CT HC'.
 				Ordering of values in all *.dih files is the same, so rows are related across all files.
-coff            amount of cofficients to calculte. Default is 4, another possibility is 3
+coff            amount of coefficient to calculte. Default is 4, another possible one is 3
+
+coffType        return the Ryckaert-Belleman coefficients instead of Fourier ones.
 
 All QM, MM and *.dih files should include data in the same order which corresponds to the mutual relationship.
 The path to those files are included in paths2files.txt
@@ -101,6 +104,10 @@ def forDerivative(M,x):
         M[i]+=[x[i]*t for t in x]
     return M
 
+def fourier2RM (k1,k2,k3,k4=0):
+    '''Return the Ryckaert-Belleman coefficients for the given Fourier ones'''
+    return k1+k3+2*k4, 3*k3-k1, 2*k2-8*k4, -4*k3, 8*k4, 0
+
 # setting the range of calculated parameters: from to (If we want to have parmeters Kij in a certain range)
 
 if len(sys.argv) == 3:
@@ -133,11 +140,20 @@ dihDir = sys.stdin.readline().strip()
 files = [f for f in os.listdir(dihDir) if f.endswith('.dih')]
 print('{} files *.dih were founded'.format(len(files)))
 
-# read amount of output cofficients
-print('What is the amount of cofficients?')
+
+# type of coefficient
+print('What type of coefficients Fourier/Ryckaert - Bellemans return?')
+coffType = sys.stdin.readline().strip()
+coffType = 'RB' if coffType == 'RB' else 'Fourier'
+print('{} coefficients will be calculated'.format(coffType))
+
+
+# read amount of output coefficients
+print('What is the amount of coefficients?')
 coff = sys.stdin.readline().strip()
 coff = 3 if coff == '3' else 4
-print('{} cofficients will be calculated'.format(coff))   
+print('{} coefficients will be calculated'.format(coff))
+
 
 diF = {k:open(dihDir + '/' + k).readline().strip() for k in files}
 
@@ -225,8 +241,16 @@ B=np.array(B)
 K=np.dot(inv(M), B)
 
 result = {type_orders[i]:list(K)[coff*i:coff*i+coff] for i in range(len(type_orders))}
-print('\nThe best result: {} parametres for each type of dihedral angle:'.format(coff))
-print(json.dumps(result, indent=1))
+
+cT = {'RB': 'Ryckaert - Bellemans', 'Fourier': 'Fourier'}
+
+
+print('\nThe best result: {} {} parametres for each type of dihedral angle:'.format(coff, cT[coffType]))
+
+if coffType == 'Fourier':
+    print(json.dumps(result, indent=1))
+else:
+    print(json.dumps({key: list(fourier2RM(*result[key])) for key in result}, indent=1))
 
 if 'fr' in locals():
     x1 = [fr] * coff * len(how_many)
@@ -234,6 +258,9 @@ if 'fr' in locals():
     ww=lsq_linear(M,B,bounds=(x1, x2)).x
     print('\n')
     resultWW = {type_orders[i]:list(ww)[coff*i:coff*i+coff] for i in range(len(type_orders))}
-    print('\nThe best result {} parametres in range ({}:{}) for each type of dihedral angle:'.format(coff, fr,to))
-    print(json.dumps(resultWW, indent=1))
+    print('\nThe best result {} {} parametres in range ({}:{}) for each type of dihedral angle:'.format(coff, cT[coffType], fr, to))
+    if coffType == 'Fourier':
+        print(json.dumps(resultWW, indent=1))
+    else:
+        print(json.dumps({key: list(fourier2RM(*resultWW[key])) for key in result}, indent=1))
 
